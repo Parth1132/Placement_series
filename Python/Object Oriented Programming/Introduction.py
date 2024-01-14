9f02661c0a3985c16bc5c9815e17f314f5d506c4
Parth1132 <parthamishra.koraput@gmail.com>
2024-01-14 12:02:43 +0530

    ADDED ATTRIBUTE

--- Python/Object Oriented Programming/Introduction.py
+++ Python/Object Oriented Programming/Introduction.py
@@ -65,18 +65,16 @@ class Item:
 class Phone(Item ):
     all=[]
     def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
-        
+        # Call to super function to have access to all attributes/methods
+        super().__init__(
+            
+        )
         #Run Validation to the received arguments
-        assert price >= 0 , f"Price {price} is not greater than or equal to zero!"
-        assert quantity >=0, f"Quantity {quantity} is not greater than or equal to zero!"
+        
         assert broken_phones >=0, f"Broken Phones {broken_phones} is not greater than or equal to zero!"
         
         # Assign to self objects
         
-        
-        self.name=name
-        self.price=price
-        self.quantity=quantity
         self.broken_phones = broken_phones
         
         #Actions to execute
@@ -88,4 +86,6 @@ print(phone1.calculate_total_price())
 
 
 phone2 = Phone("jscpjonev20",700,5,2)
+
+phone3 =Phone("jscv30",700,7)
 
